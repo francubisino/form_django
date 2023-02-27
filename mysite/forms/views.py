ac1c52cbb6cfe04a0c37cc59b189f595aaac9560
Francisco <francubisino93@gmail.com>
2023-02-27 15:09:41 -0300

    proyecto form_django

---
+++ mysite/forms/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from .forms import RegistrarUsuario
+from .models import Usuarios
+
+# Create your views here.
+
+def index(request):
+    mis_usuarios= Usuarios.objects.all()
+    if request.method == 'POST':
+       register_form = RegistrarUsuario(request.POST)
+       if register_form.is_valid():
+           success = register_form.registrar_usuario()
+           return redirect('./')       
+    else:
+        #Muestra nuestro form
+        register_form = RegistrarUsuario()
+        return render(request, 'forms/mi_form.html',
+                     {'register_form': register_form,
+                      'usuarios': mis_usuarios})
+
+    #register_form= RegistrarUsuario()
+    #return render(request, 'forms/mi_form.html', {'register_form': register_form})
+
